ecda09ad927984ca922a2eb9096ee0d950566348
hkoturap <Hemanth_Koturappa@mentor.com>
2019-08-27 14:56:16 +0530

    Update ExpenseManagerSystemTest.java

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -10,24 +10,24 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class ExpenseManagerSystemTest {
 
-	
+
 	@Test
 	public void titleTest1() throws Exception{
 		WebDriver driver;
 		driver = new HtmlUnitDriver();
-		driver.get("http://localhost:8089/ExpenseApp-1/login.jsp");	
+		driver.get("http://localhost:8089/ExpenseApp-1/login.jsp");
 		//Thread.sleep(600000);
 
 		//Buggy Code
-		String expectedTitle = "Hello Page";
-		
+		//String expectedTitle = "Hello Page";
+
 		//Correct Code
-		//String expectedTitle = "Login page";
-		
+		String expectedTitle = "Login page";
+
 		String actualTitle = driver.getTitle();
 		assertEquals(expectedTitle, actualTitle);
         System.out.println(actualTitle);
 	}
-	
-	
+
+
 }
